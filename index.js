73a0fceb9944e9c52cc3e45e8f2233eef1eed64c
fervillalbag <21fervillalbag@gmail.com>
2022-02-17 16:18:12 -0300

    feat: redux y javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+const { createStore } = require("redux");
+
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Update  State", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
